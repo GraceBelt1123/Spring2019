909c4417bfc459a938248a348d0b748164f10039
Unknown <undergo4570@gmail.com>
2019-02-19 21:55:02 -0700

    Committeed

---
+++ Unity/GrossPipe/GrossPipe/Assets/Scripts/Movement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public float defaultSpeed = 7;
+    public float currentSpeed;
+    public Rigidbody Player;
+    public bool iControl = true;
+    public float runSpeed = 13;
+
+    void Start()
+    {
+        Player = GetComponent<Rigidbody>();
+        currentSpeed = defaultSpeed;
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            currentSpeed = runSpeed;
+        }
+        else
+        {
+            currentSpeed = defaultSpeed;
+        }
+        float sugar = Input.GetAxis("Horizontal") * currentSpeed;
+        Player.transform.Translate(sugar * Time.deltaTime, 0, 0);
+    }
+}
