7c2426acb7ff873ed4996cbac3ecda00b0979b80
Unknown <undergo4570@gmail.com>
2019-02-22 16:59:10 -0700

    Committed

--- Unity/GrossPipe/GrossPipe/Assets/Scripts/Dropping.cs
+++ Unity/GrossPipe/GrossPipe/Assets/Scripts/Dropping.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.AI;
 using UnityEngine;
 
 public class Dropping : MonoBehaviour {
@@ -8,6 +9,18 @@ public class Dropping : MonoBehaviour {
 
     void OnTriggerEnter()
     {
+        StartCoroutine(Wait());
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    IEnumerator Wait()
+    {
         drop.GetComponent<Rigidbody>().useGravity = true;
+        yield return new WaitForSeconds(1);
+        drop.GetComponent<NavMeshAgent>().enabled = true;
     }
 }
