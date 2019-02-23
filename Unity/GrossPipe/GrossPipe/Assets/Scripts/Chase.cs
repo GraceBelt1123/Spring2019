240b8214c888b563dc03c3edda428cf93595f0dc
Unknown <undergo4570@gmail.com>
2019-02-22 17:03:09 -0700

    Committeed

---
+++ Unity/GrossPipe/GrossPipe/Assets/Scripts/Chase.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Chase : MonoBehaviour
+{
+    public GameObject target = null;
+    private NavMeshAgent itarget = null;
+
+    private void Start()
+    {
+        itarget = this.GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        itarget.SetDestination(target.transform.position);
+    }
+}
