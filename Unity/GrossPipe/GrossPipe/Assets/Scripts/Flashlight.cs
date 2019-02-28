0f7e5eba1f3b296e9f9efff6bbf87aef92b58791
Unknown <undergo4570@gmail.com>
2019-02-28 14:26:12 -0700

    Committeed

---
+++ Unity/GrossPipe/GrossPipe/Assets/Scripts/Flashlight.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+    public Light flashlight;
+    public bool activate;
+    public bool reduce;
+
+    private void Start()
+    {
+        flashlight = GetComponent<Light>();
+    }
+
+    private void Update()
+    {
+        flashlight.enabled = activate;
+        if(Input.GetKeyDown("f"))
+        {
+            activate = !activate;
+        }
+    }
+}
---
+++ Unity/GrossPipe/GrossPipe/Assets/Scripts/TapTap.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TapTap : MonoBehaviour
+{
+    public float iForce = 10;
+    public float Sugar = 5;
+    public Vector3 Vec;
+    public Rigidbody Player;
+    Quaternion dRotation;
+    Quaternion fRotation;
+
+    private void Start()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.rotation = fRotation;
+            Player.velocity = Vector3.zero;
+            Player.AddForce(Vector2.up * iForce, ForceMode.Force);
+       
+        }
+    }
+
+    private void Update()
+    {
+        Player = GetComponent<Rigidbody>();
+        dRotation = Quaternion.Euler(0, 0, -90);
+        fRotation = Quaternion.Euler(0, 0, 35);
+
+    }
+    
+   private void VecTap()
+    {
+        transform.rotation = Quaternion.Lerp(transform.rotation, dRotation, Sugar * Time.deltaTime);
+    }
+}
+
+
+
