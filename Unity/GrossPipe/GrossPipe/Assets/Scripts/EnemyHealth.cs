b5e5c98c141f49cbd9e7b9ce71cff26b1be0291c
Unknown <undergo4570@gmail.com>
2019-03-04 22:03:40 -0700

    Committeed

---
+++ Unity/GrossPipe/GrossPipe/Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public int health = 100;
+    public GameObject death;
+
+    public void takeDamage(int damage)
+    {
+        health -= damage;
+        if(health<= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die ()
+    {
+        Instantiate(death, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
--- Unity/GrossPipe/GrossPipe/Assets/Scripts/Shoot.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Shoot : MonoBehaviour
-{
-    /*https://www.youtube.com/watch?v=JC59tDg4tmo*/
-}
