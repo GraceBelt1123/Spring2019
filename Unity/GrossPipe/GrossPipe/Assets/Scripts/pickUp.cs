0a73493e4590baf7fda075a9c11b90b90dcec40e
Unknown <undergo4570@gmail.com>
2019-03-13 22:18:01 -0600

    Committeed

---
+++ Unity/GrossPipe/GrossPipe/Assets/Scripts/pickUp.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickUp : MonoBehaviour
+{
+    public GameObject antibodyWeapon;
+    public GameObject weapondown;
+
+    private void Start()
+    {
+        antibodyWeapon.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            antibodyWeapon.SetActive(true);
+            weapondown.SetActive(false);
+        }
+    }
+}
